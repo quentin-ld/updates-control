89b463d2493deda9d8e5aa92aa1ea59199de5354
Quentin <quentin@holdmywp.com>
2026-03-31 10:20:11 +0200

    chore: commit save point

--- .config/.eslintrc.js
+++ .config/.eslintrc.js
@@ -1,4 +1,3 @@
-// `recommended` enables Prettier when `prettier` is installed; `recommended-with-formatting` does not.
 module.exports = {
 	extends: [ 'plugin:@wordpress/eslint-plugin/recommended' ],
 	env: {
